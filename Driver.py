d6857618f174aa363592606d079c30861e48d031
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-11 17:06:01 -0700

    Created todo

---
+++ Driver.py
@@ -0,0 +1,7 @@
+# TODO: Import data set - use dictionary to store already inserted nodes.  Search dictionary before creating new node.  Create new node when there isn't a match.
+# TODO: Create command line menu with 4 options (one for each task).
+# TODO: Task 1 - Query number of cities directly connected to input
+# TODO: Task 2 - Determine whether two inputs are directly connected
+# TODO: Task 3 - Determine whether there is a k-hop connection between two inputs.  Print one solution.  k <= d
+# TODO: Task 4 - Determine whehter or not there is a connection between two inputs.  Print one solution.
+
