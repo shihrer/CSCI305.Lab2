a5796ba9439a5cbe2e63da4f57af608206fed41f
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-17 16:12:46 -0700

    Refactored to be object oriented.

--- Driver.py
+++ Driver.py
@@ -6,52 +6,58 @@
 # TODO: Task 4 - Determine whether or not there is a connection between two inputs.  Print one solution.
 
 
+class Lab_2:
+    def __init__(self, file_name):
+        self.file = file_name
 
-def task1():
-    city = input("Enter a city > ")
-    connections = str(len(d[city]))  # 'd' is the dict. Change the name to whatever you want
-    print("%s has %s connections.") % (city, connections)
+    def start_lab(self):
+        self.menu()
+
+    def task1(self):
+        city = input("Enter a city > ")
+        connections = str(len(d[city]))  # 'd' is the dict. Change the name to whatever you want
+        print("%s has %s connections.") % (city, connections)
+
+    def task2(self):
+        return 0
+
+    def task3(self):
+        return 0
+
+    def task4(self):
+        return 0
+
+    def store_cities(f):
+        for line in f:
+            if line.startswith('' or 'from' or '-'):
+                line = ''
+        # All this does is remove the extra lines. I'm not sure how you want to get and store the rest of the data.
 
 
-def task2():
-    return 0
+    # Displays menu
+    def menu(self):
+        print("To find the number of cities connected to a city, enter '1'")  # task 1
+        print("To find out whether or not there is a direct connection between two cities, enter '2'")  # task 2
+        print("To determine whether or not there is a certain length connection between two cities, enter '3'")  # task 3
+        print("To find out if there is a connection between two cities, enter '4'")  # task 4
 
+        while True:
+            choice = input("Enter your choice (1, 2, 3, or 4) > ")
+            if choice == '1':
+                self.task1()
+            elif choice == '2':
+                self.task2()
+            elif choice == '3':
+                self.task3()
+            elif choice == '4':
+                self.task4()
+            else:
+                print("That is not a valid choice. Please pick again from 1, 2, 3, or 4.")
 
-def task3():
-    return 0
+if __name__ == "__main__":
+    the_lab = Lab_2("city1.txt")
+    the_lab.start_lab()
 
-
-def task4():
-    return 0
-
-
-def store_cities(f):
-    for line in f:
-        if line.startswith('' or 'from' or '-'):
-            line = ''
-    # All this does is remove the extra lines. I'm not sure how you want to get and store the rest of the data.
-
-
-###############
-
-# Menu
-print("To find the number of cities connected to a city, enter '1'")  # task 1
-print("To find out whether or not there is a direct connection between two cities, enter '2'")  # task 2
-print("To determine whether or not there is a certain length connection between two cities, enter '3'")  # task 3
-print("To find out if there is a connection between two cities, enter '4'")  # task 4
-
-while True:
-    choice = input("Enter your choice (1, 2, 3, or 4) > ")
-    if choice == '1':
-        task1()
-    elif choice == '2':
-        task2()
-    elif choice == '3':
-        task3()
-    elif choice == '4':
-        task4()
-    else:
-        print("That is not a valid choice. Please pick again from 1, 2, 3, or 4.")
 
 
 
