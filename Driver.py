b86cf42247d37d577b782247137b01acbbaf8d9b
Michael Shihrer <michael.shihrer@gmail.com>
2016-03-17 20:10:19 -0700

    Completed task 2

--- Driver.py
+++ Driver.py
@@ -5,6 +5,8 @@
 import networkx as nx
 import re
 
+import sys
+
 
 class Lab_2:
     def __init__(self, file_name):
@@ -37,57 +39,87 @@ class Lab_2:
                     result = re.split("\s\s+", line)
                     yield result[0], result[1], int(result[2])
 
-    def task1(self):
+    def task1(self, city):
         """
         Find the number of cities directly connected to a given city.
         """
-        city = input("Enter a city > ")
         city_neighbors = self.graph.neighbors(city.lower())
         print("{} has {} connections.".format(city, len(city_neighbors)))
 
-    def task2(self):
+    def task2(self, source, destination):
         """
         :return: Yes/No string
         Determine if there is a direct connection between two cities.
         """
 
+        if destination in self.graph.neighbors(source):
+            return "YES"
+        else:
+            return "NO"
 
-        return 0
-
-    def task3(self, d):
+    def task3(self, source, destination, d):
         """
+        :param source: source city string
+        :param destination: destination city string
         :param d: Integer for d-hop
         :return: Yes/No string and solution
         Determine if there is a k-hop connection between two cities.
         """
         return 0
 
-    def task4(self):
+    def task4(self, source, destination):
+        """
+        :param source: source city string
+        :param destination: destination city string
+        :return: Yes/No string and solution
         """
 
-        :return: Yes/No string and solution
-        Determine if there is a connection between two cities and output one out.
-        """
         return 0
 
     def menu(self):
-        print("To find the number of cities connected to a city, enter '1'")  # task 1
-        print("To find out whether or not there is a direct connection between two cities, enter '2'")  # task 2
-        print("To determine whether or not there is a certain length connection between two cities, enter '3'")  # task 3
-        print("To find out if there is a connection between two cities, enter '4'")  # task 4
-
         while True:
-            choice = input("Enter your choice (1, 2, 3, or 4) > ")
+            print("To find the number of cities connected to a city, enter '1'")  # task 1
+            print("To find out whether or not there is a direct connection between two cities, enter '2'")  # task 2
+            print("To determine whether or not there is a certain length connection between two cities, enter '3'")  # task 3
+            print("To find out if there is a connection between two cities, enter '4'")  # task 4
+            print("To exit, enter 'e'") # Exit program
+            print()
+            choice = input("Enter your choice (1, 2, 3, 4, or e) > ")
+            print()
+
             if choice == '1':
-                self.task1()
+                self.task1(input("Enter a city > "))
+                print()
             elif choice == '2':
-                self.task2()
+                source = input("Enter a source city > ")
+                destination = input("Enter a destination city > ")
+                print(self.task2(source.lower(),destination.lower()))
+                print()
             elif choice == '3':
-                self.task3()
+                source = input("Enter a source city > ")
+                destination = input("Enter a destination city > ")
+
+                valid_input = False
+                while not valid_input:
+                    int_input = input("Enter a d-hop > ")
+                    valid_input = self.is_valid_input(int_input)
+
+                self.task3(source, destination, int_input)
             elif choice == '4':
                 self.task4()
+            elif choice.lower() == 'e':
+                sys.exit()
             else:
-                print("That is not a valid choice. Please pick again from 1, 2, 3, or 4.")
+                print("That is not a valid choice. Please pick again from 1, 2, 3, 4, or e.")
+                print()
+
+    @staticmethod
+    def is_valid_input(test_input):
+        try:
+            int(test_input)
+            return True
+        except ValueError:
+            return False
 
 if __name__ == "__main__":
     the_lab = Lab_2("city1.txt")
